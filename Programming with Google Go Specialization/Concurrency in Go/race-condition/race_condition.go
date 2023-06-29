97798a5f6f1e046fc74111a57805201bfc760500
meowpunch <jinhoonbae@gmail.com>
2023-06-29 20:28:45 +0200

    race-condition

--- Programming with Google Go Specialization/Concurrency in Go/race-condition/race_condition.go
+++ Programming with Google Go Specialization/Concurrency in Go/race-condition/race_condition.go
@@ -5,22 +5,35 @@ import (
 	"time"
 )
 
+/*
+- What is race condition?
+A race condition is a problem that can happen as a result of all these possible interleaving that you have to consider.
+More precisely, the result of program is non-deterministic. It means that the output can be different by execution even if the input is the same.
+
+- How it can occur?
+Race condition occurs due to communication. Especially in terms of go and goroutine, it happens when goroutine communicates on shared variable.
+
+Let's look at the below code. Goroutine 1 and goroutine 2 communicates on shared variable x.
+Mostly the result of `2 == findMax(integers)` is `true`. It can be `false`. And if there are more goroutines run function `increaseXAndPrintX`. The result is mostly `false`
+*/
 func main() {
+	// shared variable
 	x := 0
-	integers := make([]int, 0, 100000)
+	integers := make([]int, 0, 2)
 
 	increaseXAndPrintX := func() {
 		x = x + 1
 		integers = append(integers, x)
 	}
 
-	for i := 0; i < 100000; i++ {
-		go increaseXAndPrintX()
-	}
+	// goroutine 1
+	go increaseXAndPrintX()
+	// goroutine 2
+	go increaseXAndPrintX()
 
 	time.Sleep(100 * time.Millisecond)
 
-	fmt.Printf("%d != %d because of race condition\n", 99999, findMax(integers))
+	fmt.Println(2 == findMax(integers))
 }
 
 func findMax(numbers []int) int {
