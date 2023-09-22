01d276810193fefde3caa149bcb0508f54056cdf
meowpunch <jinhoonbae@gmail.com>
2023-09-22 16:08:06 +0200

    add http server

--- Playground/Fx - dependency injection/main.go
+++ Playground/Fx - dependency injection/main.go
@@ -1,7 +1,37 @@
 package main
 
-import "go.uber.org/fx"
+import (
+	"context"
+	"fmt"
+	"go.uber.org/fx"
+	"net"
+	"net/http"
+)
 
 func main() {
-	fx.New().Run()
+	fx.New(
+		fx.Provide(NewHTTPServer),
+		fx.Invoke(func(*http.Server) {}),
+	).Run()
+}
+
+// NewHTTPServer builds an HTTP server that will begin serving requests
+// when the Fx application starts.
+func NewHTTPServer(lc fx.Lifecycle) *http.Server {
+	srv := &http.Server{Addr: ":8080"}
+	lc.Append(fx.Hook{
+		OnStart: func(ctx context.Context) error {
+			ln, err := net.Listen("tcp", srv.Addr)
+			if err != nil {
+				return err
+			}
+			fmt.Println("Starting HTTP server at", srv.Addr)
+			go srv.Serve(ln)
+			return nil
+		},
+		OnStop: func(ctx context.Context) error {
+			return srv.Shutdown(ctx)
+		},
+	})
+	return srv
 }
