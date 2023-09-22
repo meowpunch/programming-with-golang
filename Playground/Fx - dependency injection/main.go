31049a554836bcd58539af438e49cce9071fca01
meowpunch <jinhoonbae@gmail.com>
2023-09-22 16:20:12 +0200

    expose endpoint with handler and router

--- Playground/Fx - dependency injection/main.go
+++ Playground/Fx - dependency injection/main.go
@@ -4,21 +4,23 @@ import (
 	"context"
 	"fmt"
 	"go.uber.org/fx"
+	"io"
 	"net"
 	"net/http"
+	"os"
 )
 
 func main() {
 	fx.New(
-		fx.Provide(NewHTTPServer),
+		fx.Provide(NewHTTPServer, NewEchoHandler, NewServeMux),
 		fx.Invoke(func(*http.Server) {}),
 	).Run()
 }
 
 // NewHTTPServer builds an HTTP server that will begin serving requests
 // when the Fx application starts.
-func NewHTTPServer(lc fx.Lifecycle) *http.Server {
-	srv := &http.Server{Addr: ":8080"}
+func NewHTTPServer(lc fx.Lifecycle, mux *http.ServeMux) *http.Server {
+	srv := &http.Server{Addr: ":8080", Handler: mux}
 	lc.Append(fx.Hook{
 		OnStart: func(ctx context.Context) error {
 			ln, err := net.Listen("tcp", srv.Addr)
@@ -34,4 +36,28 @@ func NewHTTPServer(lc fx.Lifecycle) *http.Server {
 		},
 	})
 	return srv
+}
+
+// EchoHandler is an http.Handler that copies its request body
+// back to the response.
+type EchoHandler struct{}
+
+// NewEchoHandler builds a new EchoHandler.
+func NewEchoHandler() *EchoHandler {
+	return &EchoHandler{}
+}
+
+// ServeHTTP handles an HTTP request to the /echo endpoint.
+func (*EchoHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	if _, err := io.Copy(w, r.Body); err != nil {
+		fmt.Fprintln(os.Stderr, "Failed to handle request:", err)
+	}
+}
+
+// NewServeMux builds a ServeMux that will route requests
+// to the given EchoHandler.
+func NewServeMux(echo *EchoHandler) *http.ServeMux {
+	mux := http.NewServeMux()
+	mux.Handle("/echo", echo)
+	return mux
 }
