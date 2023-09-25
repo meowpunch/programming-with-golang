e1f1393c3fa0ad00e2500af35e4a9b6efc1a4116
meowpunch <jinhoonbae@gmail.com>
2023-09-25 12:16:33 +0200

    refactor for DI as a group

--- Playground/Fx - dependency injection/main.go
+++ Playground/Fx - dependency injection/main.go
@@ -13,10 +13,11 @@ import (
 
 // NewServeMux builds a ServeMux that will route requests
 // to the given EchoHandler.
-func NewServeMux(route1, route2 Route) *http.ServeMux {
+func NewServeMux(routes []Route) *http.ServeMux {
 	mux := http.NewServeMux()
-	mux.Handle(route1.Pattern(), route1)
-	mux.Handle(route2.Pattern(), route2)
+	for _, route := range routes {
+		mux.Handle(route.Pattern(), route)
+	}
 	return mux
 }
 
@@ -99,6 +100,16 @@ func NewHTTPServer(lc fx.Lifecycle, mux *http.ServeMux, log *zap.Logger) *http.S
 	return srv
 }
 
+// AsRoute annotates the given constructor to state that
+// it provides a route to the "routes" group.
+func AsRoute(f any) any {
+	return fx.Annotate(
+		f,
+		fx.As(new(Route)),
+		fx.ResultTags(`group:"routes"`),
+	)
+}
+
 func main() {
 	fx.New(
 		fx.WithLogger(func(log *zap.Logger) fxevent.Logger {
@@ -106,14 +117,12 @@ func main() {
 		}),
 		fx.Provide(
 			NewHTTPServer,
-			fx.Annotate(NewServeMux,
-				fx.ParamTags(`name:"echo"`, `name:"hello"`)),
-			fx.Annotate(NewEchoHandler,
-				fx.As(new(Route)),
-				fx.ResultTags(`name:"echo"`)),
-			fx.Annotate(NewHelloHandler,
-				fx.As(new(Route)),
-				fx.ResultTags(`name:"hello"`)),
+			fx.Annotate(
+				NewServeMux,
+				fx.ParamTags(`group:"routes"`),
+			),
+			AsRoute(NewEchoHandler),
+			AsRoute(NewHelloHandler),
 			zap.NewExample,
 		),
 		fx.Invoke(func(*http.Server) {}),
