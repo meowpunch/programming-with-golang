ef1891ef8c3695603289c2c0dd0129e851d8fa24
meowpunch <jinhoonbae@gmail.com>
2023-09-25 11:01:51 +0200

    Refactor

--- Playground/Fx - dependency injection/main.go
+++ Playground/Fx - dependency injection/main.go
@@ -2,23 +2,80 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"go.uber.org/fx"
+	"go.uber.org/fx/fxevent"
 	"go.uber.org/zap"
 	"io"
 	"net"
 	"net/http"
 )
 
-func main() {
-	fx.New(
-		fx.Provide(
-			NewHTTPServer,
-			NewEchoHandler,
-			NewServeMux,
-			zap.NewExample,
-		),
-		fx.Invoke(func(*http.Server) {}),
-	).Run()
+// NewServeMux builds a ServeMux that will route requests
+// to the given EchoHandler.
+func NewServeMux(route1, route2 Route) *http.ServeMux {
+	mux := http.NewServeMux()
+	mux.Handle(route1.Pattern(), route1)
+	mux.Handle(route2.Pattern(), route2)
+	return mux
+}
+
+// Route is an http.Handler that knows the mux pattern
+// under which it will be registered.
+type Route interface {
+	http.Handler
+
+	// Pattern reports the path at which this is registered.
+	Pattern() string
+}
+
+// EchoHandler is an http.Handler that copies its request body
+// back to the response.
+type EchoHandler struct {
+	log *zap.Logger
+}
+
+func (*EchoHandler) Pattern() string {
+	return "/echo"
+}
+
+// NewEchoHandler builds a new EchoHandler.
+func NewEchoHandler(log *zap.Logger) *EchoHandler {
+	return &EchoHandler{log: log}
+}
+
+// ServeHTTP handles an HTTP request to the /echo endpoint.
+func (h *EchoHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	if _, err := io.Copy(w, r.Body); err != nil {
+		h.log.Warn("Failed to handle request", zap.Error(err))
+	}
+}
+
+type HelloHandler struct {
+	log *zap.Logger
+}
+
+func (*HelloHandler) Pattern() string {
+	return "/hello"
+}
+
+func NewHelloHandler(log *zap.Logger) *HelloHandler {
+	return &HelloHandler{log: log}
+}
+
+func (h *HelloHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	body, err := io.ReadAll(r.Body)
+	if err != nil {
+		h.log.Error("Failed to read request", zap.Error(err))
+		http.Error(w, "Internal server error", http.StatusInternalServerError)
+		return
+	}
+
+	if _, err := fmt.Fprintf(w, "Hello, %s\n", body); err != nil {
+		h.log.Error("Failed to write response", zap.Error(err))
+		http.Error(w, "Internal server error", http.StatusInternalServerError)
+		return
+	}
 }
 
 // NewHTTPServer builds an HTTP server that will begin serving requests
@@ -42,28 +99,23 @@ func NewHTTPServer(lc fx.Lifecycle, mux *http.ServeMux, log *zap.Logger) *http.S
 	return srv
 }
 
-// EchoHandler is an http.Handler that copies its request body
-// back to the response.
-type EchoHandler struct {
-	log *zap.Logger
-}
-
-// NewEchoHandler builds a new EchoHandler.
-func NewEchoHandler(log *zap.Logger) *EchoHandler {
-	return &EchoHandler{log: log}
-}
-
-// ServeHTTP handles an HTTP request to the /echo endpoint.
-func (h *EchoHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	if _, err := io.Copy(w, r.Body); err != nil {
-		h.log.Warn("Failed to handle request", zap.Error(err))
-	}
-}
-
-// NewServeMux builds a ServeMux that will route requests
-// to the given EchoHandler.
-func NewServeMux(echo *EchoHandler) *http.ServeMux {
-	mux := http.NewServeMux()
-	mux.Handle("/echo", echo)
-	return mux
+func main() {
+	fx.New(
+		fx.WithLogger(func(log *zap.Logger) fxevent.Logger {
+			return &fxevent.ZapLogger{Logger: log}
+		}),
+		fx.Provide(
+			NewHTTPServer,
+			fx.Annotate(NewServeMux,
+				fx.ParamTags(`name:"echo"`, `name:"hello"`)),
+			fx.Annotate(NewEchoHandler,
+				fx.As(new(Route)),
+				fx.ResultTags(`name:"echo"`)),
+			fx.Annotate(NewHelloHandler,
+				fx.As(new(Route)),
+				fx.ResultTags(`name:"hello"`)),
+			zap.NewExample,
+		),
+		fx.Invoke(func(*http.Server) {}),
+	).Run()
 }
